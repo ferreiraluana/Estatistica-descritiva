4b47578021531a0dbddaab7c6ef05a921d5bfb9f
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-10-29 16:51:39 -0300

    Update main.r

--- main.r
+++ main.r
@@ -1,3 +1,11 @@
-#Display table
+# Display table
 file <- read.csv("table.csv", sep = ",")
 print(file)
+
+# Display specific column
+print(file$Nome)
+print(file$Altura.m.)
+
+# Also an option
+file_peso <- file$Peso.kg.
+print(file_peso)
