80324a23884a2f1cd40471036d0470c465942915
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-10-29 17:35:56 -0300

    Update questao1letraH.r

--- questao1letraH.r
+++ questao1letraH.r
@@ -4,8 +4,6 @@ file <- read.csv("table.csv",TRUE, sep = ",")
 
 altura <- hist(file$Altura.m., main = "Frequência de Altura", xlab = "Altura", ylab = "Quantidade de Pessoas")
 peso <- hist(file$Peso.kg., main = "Frequência Peso", xlab = "Peso(kg)", ylab = "Quantidade de Pessoas")
-# escolaridade ...
 
 print(altura)
 print(peso)
-# print(escolaridade)
