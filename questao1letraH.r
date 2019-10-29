1d758cecf72fafd9178f72d2a268f4dc6a5b9079
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-10-29 17:21:53 -0300

    Create questao1letraH.r

---
+++ questao1letraH.r
@@ -0,0 +1,11 @@
+# Display histograms
+
+file <- read.csv("table.csv",TRUE, sep = ",")
+
+altura <- hist(file$Altura.m., main = "Frequência de Altura", xlab = "Altura", ylab = "Quantidade de Pessoas")
+peso <- hist(file$Peso.kg., main = "Frequência Peso", xlab = "Peso(kg)", ylab = "Quantidade de Pessoas")
+# escolaridade ...
+
+print(altura)
+print(peso)
+# print(escolaridade)
