55037dcb52a7cd97779892aafab1b14d56339e90
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-10-30 15:30:59 -0300

    Update questao1letraB.r

--- questao1letraB.r
+++ questao1letraB.r
@@ -3,7 +3,7 @@
 file <- read.csv("tablej.csv",TRUE,sep = ",")
 file$IMC<- (file$Peso/(file$Altura)^2)
 
- # Display absolute frequencies
+# Display absolute frequencies
 
 Peso.kg.frequence <- table(file$Peso.kg.)
 print("Tabela de frequência absoluta do Peso(kg):")
@@ -17,14 +17,13 @@ IMC.frequence <- table(file$IMC)
 print("Tabela de frequência absoluta do IMC:")
 print(IMC.frequence)
 
- # Display relative frequencies
+# Display IMC relative frequence
  
-freq_rel <- prop.table(freq_imc)
-print(freq_rel)
- # Display the percentage of relative frequency
-print(100 * freq_rel)
- # Display the table of frequencies
-> brk<-seq(18,30,3)
-> print(brk)
-> classes<-c("18-20","21-23","24-26","27-30")
-> table(cut(file$IMC,breaks=brk,right=FALSE,labels=classes))
+IMC.relative.frequence <- prop.table(IMC.frequence)
+print("Tabela de frequência relativa do IMC:")
+print(IMC.relative.frequence)
+
+# Display the percentage of IMC relative frequence
+
+IMC.percentage.relative.frequence <- IMC.relative.frequence*100
+print(IMC.percentage.relative.frequence)
