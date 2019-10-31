86e43f5ca720c63da1ad1f83fa26bcc011e743b0
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-10-31 13:30:51 -0300

    Delete questao1letraC.r

--- questao1letraC.r
+++
@@ -1,7 +0,0 @@
-file <- read.csv("table.csv",TRUE, sep = ",")
-
-print("O peso médio das pessoas é:")
-print(mean(file$Peso.kg.))
-
-print("A altura média das pessoas é:")
-print(mean(file$Altura.m.))
