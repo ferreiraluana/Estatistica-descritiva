0c675c38cce362f484540682d97cfce6b0be78e7
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-10-31 13:40:49 -0300

    Delete questao1letraH.r

--- questao1letraH.r
+++
@@ -1,9 +0,0 @@
-# Display histograms
-
-file <- read.csv("table.csv",TRUE, sep = ",")
-
-altura <- hist(file$Altura.m., main = "Frequência de Altura", xlab = "Altura", ylab = "Quantidade de Pessoas")
-peso <- hist(file$Peso.kg., main = "Frequência Peso", xlab = "Peso(kg)", ylab = "Quantidade de Pessoas")
-
-print(altura)
-print(peso)
